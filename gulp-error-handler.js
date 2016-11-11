00204764cce1c4af515d3ea972b6e36e41d0919d
SteffanDonal <steffan@enigma-marketing.co.uk>
2016-11-11 11:04:47 +0000

    Improve plumber error formatting.

--- gulp-error-handler.js
+++ gulp-error-handler.js
@@ -1,7 +1,18 @@
 /*jslint node:true */
 'use strict';
 
+var chalk = require('chalk');
+
 module.exports = function (error) {
-    console.error(error.toString());
+    if (chalk.hasColor(error.message)) {
+        console.error(error.message);
+    } else {
+        console.error('\n' +
+            chalk.green(error.plugin) + ':\n    ' +
+            chalk.white.bgRed(error.name) + '\n    ' +
+            error.message.trim().replace(/\n/g, '\n    ') +
+        '\n');
+    }
+
     this.emit('end');
 };
